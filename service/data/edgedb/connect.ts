0b9980e75f7e207cd456c6d7431e6929602c645d
Roberto Prevato <roberto.prevato@gmail.com>
2020-06-09 09:36:34 +0200

    adds password to edgedb connection and file describing env variables

--- service/data/edgedb/connect.ts
+++ service/data/edgedb/connect.ts
@@ -9,6 +9,7 @@ export async function connect(): Promise<AwaitConnection> {
   return await edgedb({
     user: getEnvSettingOrThrow("EDGEDB_USER"),
     host: getEnvSettingOrThrow("EDGEDB_HOST"),
+    password: getEnvSettingOrThrow("EDGEDB_PASSWORD"),
     database: "cla",
   });
 }
