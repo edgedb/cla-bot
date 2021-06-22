1eb3a967cefb9f96497c783404cb7c1eda6f0556
Elvis Pranskevichus <elvis@edgedb.com>
2021-06-22 11:07:03 -0700

    Unbreak Github login callback
    The "state=" query parameter may not always be the last in the
    redirect URL, so use structured URL manipulation instead of
    mucking around with strings.

--- pages/api/contributors/auth/github/index.ts
+++ pages/api/contributors/auth/github/index.ts
@@ -10,16 +10,12 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(400).end("Missing state context.");
   }
 
-  const signInUrl = githubAuth.code.getUri();
-
-  if (signInUrl.endsWith("&state=") === false) {
-    // Something changed in the library used for OAuth
-    throw new Error(`Breaking change in the library used for OAuth.
-      Expected a url ending with "state=". Verify what is the actual value: we need
-      to pass a query string parameter inside the redirect location.
-    `);
+  if (Array.isArray(state)) {
+    throw new Error(`Unexpected multiple "state=" parameters in callback URI`);
   }
 
-  res.setHeader("Location", signInUrl + state);
+  const signInUrl = new URL(githubAuth.code.getUri());
+  signInUrl.searchParams.set("state", state);
+  res.setHeader("Location", signInUrl.toString());
   res.status(302).end();
 };
