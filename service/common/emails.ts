d8e5c8cc5ad4620554fe01416f294f526e75074e
Elvis Pranskevichus <elvis@edgedb.com>
2023-05-19 15:29:38 -0700

    Drop email validation
    Email validation is a fool's errand and doesn't really serve any meaningful
    purpose, since the bot treats emails as unique identifiers and does not send
    any emails.
    
    Fixes: #47

--- service/common/emails.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Validates a single email address value.
- */
-export function validateEmail(value: string): boolean {
-  /* tslint:disable-next-line */
-  return /^([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.\+]+)\.([a-zA-Z]{2,5}){1,25}$/.test(
-    value
-  );
-}
